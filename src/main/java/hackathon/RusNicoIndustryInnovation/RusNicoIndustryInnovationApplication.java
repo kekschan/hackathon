9b31b757bf3b56c9f795615de997d104c32a0aee
Denis Borshev <dnsborshev@gmail.com>
2023-01-19 15:31:37 +0300

    Создание проекта

---
+++ src/main/java/hackathon/RusNicoIndustryInnovation/RusNicoIndustryInnovationApplication.java
@@ -0,0 +1,13 @@
+package hackathon.RusNicoIndustryInnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RusNicoIndustryInnovationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RusNicoIndustryInnovationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hackathon/RusNicoIndustryInnovation/RusNicoIndustryInnovationApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.RusNicoIndustryInnovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RusNicoIndustryInnovationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
